67ec6f8ac75e8b464ba8207b8dd77dc604e577f6
rodmirsantana <rodmirandasantana@gmail.com>
2024-05-20 14:01:06 -0300

    update dependencies

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ require("express-async-errors");
 
 const cors = require("./app/middlewares/cors");
 const errorHandler = require("./app/middlewares/errorHandler");
-const routes = require("./routes");
+const routes = require('./routes');
 
 const app = express();
 
@@ -12,6 +12,4 @@ app.use(cors);
 app.use(routes);
 app.use(errorHandler);
 
-app.listen(3001, () =>
-  console.log("🔥 Server started at http://localhost:3001")
-);
+app.listen(3001, () => console.log("🔥 Server started at http://localhost:3001"));
